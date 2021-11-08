76ceda9ededf1c70dba64249fbc9a2ed8203bd4d
Gaowei Xu <gaowexu@amazon.com>
2021-11-08 11:08:26 +0800

    add stack name as parameter

--- source/bin/ipc-ai-saas.ts
+++ source/bin/ipc-ai-saas.ts
@@ -5,7 +5,13 @@ import { IpcAiSaasStack } from '../lib/ipc-ai-saas-stack';
 import { BootstraplessStackSynthesizer } from 'cdk-bootstrapless-synthesizer';
 
 const app = new cdk.App();
-new IpcAiSaasStack(app, 'IpcAiSaasStack', { synthesizer: newSynthesizer() });
+new IpcAiSaasStack(
+    app,
+    'IpcAiSaasStack',
+    {
+      stackName: process.env.STACK_NAME,
+      synthesizer: newSynthesizer()
+    });
 
 app.synth();
 
